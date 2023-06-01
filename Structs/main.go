1ebddd78b1b0e0b0d65f277d2ef03de08a82e56f
pstod <pstodmal2000@gmail.com>
2023-06-01 18:11:47 +0530

    Pointers and stucts

--- Structs/main.go
+++ Structs/main.go
@@ -17,9 +17,21 @@ func main() {
 
 	p := person{"Pd", 23, address{"Pune", 90908}}
 	p.printout()
+	fmt.Println()
+	p.updatename("lol") //shortcut : go converts person to &person due to receiver type
+	fmt.Println(p)
+	(&p).updatename("mol")
+	fmt.Println(p)
 }
 
 func (p person) printout() {
 
 	fmt.Println(p)
+	fmt.Printf("%+v", p)
+}
+
+func (p *person) updatename(b string) {
+
+	(*p).name = b
+
 }
